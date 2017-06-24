68b44771ed3bbe02706870945505f2b6e9890308
yty3592 <yty3592@gmail.com>
2017-06-24 21:32:42 +0900

    위키워드 express 설정 및 디렉토리 정리

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var index = require('./routes/index');
+var ejs = require('ejs');
 var app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'public/views'));
 app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+app.engine('html', ejs.renderFile);
 
 app.use('/', index);
 app.listen(3000, () => { console.log('Example app listening on port 3000!'); });
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,4 @@ router.get('/', function(req, res, next) {
   res.render('index.html');
 });
 
-
-
 module.exports = router;
