31ddb222843f1431218dc1a859aabc656a083075
ywnwalone <ywnwalone@naver.com>
2017-06-29 20:01:32 +0900

    facebook oauth/ naver api

---
+++ public/javascripts/axios_test.js
@@ -0,0 +1,59 @@
+
+
+document.addEventListener('DOMContentLoaded',()=>{
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1974940292729310',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    loginCheck();
+  };
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+});
+
+
+function loginCheck(){
+  let signin = document.querySelector('.sign-in');
+  let username = document.querySelector('.username');
+  FB.api('/me',response=>{
+    if(response.id){
+      //signin.style.display = 'none';
+      signin.classList.add('invisible');
+      username.innerHTML = response.name+' 님';
+    }else{
+      //signin.style.display = "inline-block";
+      signin.classList.remove('invisible');
+      username.innerHTML = '';
+    }
+    console.log(response);
+  });
+}
+
+function login(){
+  let signin = document.querySelector('#sign-in');
+  FB.login(function(response) {
+     console.log(response);
+     if (response.status === 'connected') {
+       console.log('connected !');
+       signin.checked=false;
+       loginCheck();
+     } else {
+       console.log('Problem!!')
+     }
+   }, {scope: 'public_profile, email'});
+}
+
+function logout(){
+  FB.logout(function(response){
+    console.log(response);
+    loginCheck();
+  });
+}
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var CLIENT_ID = 'OtHO5WKtYeTypxuKxi7a';
+var CLIENT_SECRET = 'Wz7sJF6cH_';
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index.html');
 });
+
+router.get('/search',function(req, res){
+  var Query = '아이유';
+  var api_url = 'https://openapi.naver.com/v1/search/news?start=2&display=100&query='
+                + encodeURI(Query);
+  var request = require('request');
+  var options = {
+    url: api_url,
+    headers: {'X-naver-Client-Id':CLIENT_ID, 'X-naver-Client-Secret': CLIENT_SECRET},
+
+  };
+
+  request.get(options, function(error, response, body){
+    if(!error && response.statusCode == 200){
+      res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+      res.end(body);
+    }else{
+      res.status(response.statusCode).end();
+      console.log('Error = '+response.statusCode);
+    }
+  });
+
+})
 
 module.exports = router;
