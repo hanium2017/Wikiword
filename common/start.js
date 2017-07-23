2744fa3d01add0d2f984cab31691a146b53cf43e
YOON-PC\Yoon <yty3592@gmail.com>
2017-07-23 13:15:48 +0900

    서버 테스트 중

--- common/start.js
+++ common/start.js
@@ -1,7 +1,8 @@
+  var HOST = 'localhost';
   require('../wikiword/app').app.listen(3000, () => { console.log('wikiword Server start port 3000!'); });
-  require('../wikipedia/app').app.listen(11000, () => { console.log('wikipedia JSON listening on port 11000!');  });
-  require('../Naver/app').app.listen(12000, () => { console.log('Naver Open API listening on port 12000!'); });
-  require('../pinterest/app').app.listen(13000, () => { console.log('pinterest JSON listening on port 13000!');  });
-  require('../Youtube/app').app.listen(14000, () => { console.log('Youtube JSON listening on port 14000!'); });
-  require('../twitter/app').app.listen(15000, () => { console.log('twitter JSON listening on port 15000!');  });
-  require('../DBpia/app').app.listen(16000, () => { console.log('DBpia JSON listening on port 16000!');  });
+  require('../wikipedia/app').app.listen(11000,HOST, () => { console.log('wikipedia JSON listening on port 11000!');  });
+  require('../Naver/app').app.listen(12000,HOST, () => { console.log('Naver Open API listening on port 12000!'); });
+  require('../pinterest/app').app.listen(13000,HOST, () => { console.log('pinterest JSON listening on port 13000!');  });
+  require('../Youtube/app').app.listen(14000,HOST, () => { console.log('Youtube JSON listening on port 14000!'); });
+  require('../twitter/app').app.listen(15000,HOST, () => { console.log('twitter JSON listening on port 15000!');  });
+  require('../DBpia/app').app.listen(16000,HOST, () => { console.log('DBpia JSON listening on port 16000!');  });
