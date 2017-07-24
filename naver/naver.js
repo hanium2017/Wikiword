a2ace7fc44d96600f31d46471ccdb3c2040b668e
YOON-PC\Yoon <yty3592@gmail.com>
2017-07-24 16:47:07 +0900

    디렉토리 정리 완료

---
+++ naver/naver.js
@@ -0,0 +1,16 @@
+exports.analyzeJSON = function (body, type){
+  const JSONArray = [];
+  let items = JSON.parse(body).items;
+
+  if(items.length === 0){
+    JSONArray.push({"message":"검색 된 자료가 없습니다."});
+  } else {
+    let deleteAttribute = (( type == 'news') ? 'originallink' : 'isbn');
+    for(var i = 0, max = items.length; i < max; i++){
+      let JSONObject = items[i];
+      delete JSONObject[deleteAttribute];
+      JSONArray.push(JSONObject);
+    }
+  }
+  return JSON.stringify(JSONArray);
+}
---
+++ youtube/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+, app = express()
+, bodyParser = require('body-parser')
+, request = require('request')
+, CORS = require('cors')()
+, youtube = require('./youtube')
+, API_KEY = require('../common/wikiword').API_KEY;
+
+app.use(CORS);
+app.get('/youtube', function(req, res, next) {
+  let url = 'https://www.googleapis.com/youtube/v3/search?part=snippet' +
+  '&maxResults=5&order=viewCount&type=video&videoDefinition=high' +
+  '&q=' + encodeURI(req.query.search, "utf-8") + '&key=' + API_KEY.google_api + '&pageToken=' + req.query.pageNum;
+  request(url, function(err, response, body) {
+
+    if (!err && response.statusCode == 200) {
+      res.writeHead(200, {'Content-Type': 'text/json;charset=utf-8'});
+      res.end(JSON.stringify(youtube.analyzeJSON(body)));
+    } else {
+      res.status(response.statusCode).end();
+      console.log('Error = ' + response.statusCode);
+    }
+  });
+});
+
+exports.app = app;
+
+
+//실제 영상링크 걸기 -> https://www.youtube.com/watch?v=' + video_id
+//pageToken에 nextPageToken  또는 prevPageToken 넣으면 된다.
+// 맨 처음에는 토큰 값 ''으로 보내고 다음부터 nextpagetoken값이 있으면 그때는 토큰값으로 보낸다.
+// 넘겨야 하는 값
+// vedioid, nextpagetoken, title, thumbnails링크,
