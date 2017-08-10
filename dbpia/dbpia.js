b179abc6b102cea89f3ea4c217d92ddfd12d9c4e
YOON-PC\Yoon <yty3592@gmail.com>
2017-08-10 11:50:21 +0900

    dbpia json 데이터 수정 완료

--- dbpia/dbpia.js
+++ dbpia/dbpia.js
@@ -6,14 +6,22 @@ exports.analyzeXML = function (xml) {
       JSONArray.push({'message': '검색 된 자료가 없습니다.'})
     } else {
       const items = obj.root.result[0].items[0].item
+
       for (let item of items) {
         const object = {}
         object.title = item.title[0].replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, '')
-        object.authors = item.authors
-        object.pages = (item.hasOwnProperty('page')) ? item.pages[0] : ''
+        object.authors = [];
+
+        let authors = item.authors[0].author
+        for(let author of authors){
+          object.authors.push(author.name[0])
+        }
+        
+        object.pages = (item.hasOwnProperty('pages')) ? item.pages[0] : ''
         object.pubDate = (item.issue[0].hasOwnProperty('yymm')) ? item.issue[0].yymm[0] : ''
-        object.link_url = item.link_url
-        object.publisher = item.publisher
+        object.link_url = item.link_url[0]
+        object.publisher = item.publisher[0].name[0]
+        object.publication = item.publication[0].name[0]
         JSONArray.push(object)
       }
     }
