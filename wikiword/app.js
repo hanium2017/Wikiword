1ac69814a787cc95f042cc8f605042fede0f0f81
YOON-PC\Yoon <yty3592@gmail.com>
2017-08-13 02:17:59 +0900

    디렉토리 정리 및 병합

--- wikiword/app.js
+++ wikiword/app.js
@@ -2,36 +2,36 @@
  *  필요 모듈 세팅 및 환경 설정 부분
  */
 const express = require('express'),
-  path = require('path'),
-  bodyParser = require('body-parser'),
-  swig = require('swig'),
-  express_session = require('express-session'),
-  cookieParser = require('cookie-parser'),
-  CORS = require('cors')(),
-  morgan = require('morgan'),
-  app = express(),
-  dbPool = require('../common/dbconnection').dbPool
+    path = require('path'),
+    bodyParser = require('body-parser'),
+    swig = require('swig'),
+    express_session = require('express-session'),
+    cookieParser = require('cookie-parser'),
+    CORS = require('cors')(),
+    morgan = require('morgan'),
+    app = express(),
+    dbPool = require('../common/dbconnection').dbPool
 
 app.use(CORS)
 app.use(morgan(':date'))
 app.use(express.static(path.join(__dirname, 'public')))
-app.set('views', path.join(__dirname, 'public/views'))
+app.set('views', [ path.join(__dirname + '/public/views/index'), path.join(__dirname + '/public/views/result')])
 app.engine('html', swig.renderFile)
 app.set('view engine', 'html')
 
 app.use(cookieParser())
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(express_session({
-  key: 'sid', // 세션키
-  secret: 'secret' // 세션 비밀키
+    key: 'sid', // 세션키
+    secret: 'secret' // 세션 비밀키
 }))
 
 /**
  * 라우트 선언 부분
  */
 const index = require('./routes/index')(app),
-  signInOut = require('./routes/signInOut')(app, dbPool)
+    signInOut = require('./routes/signInOut')(app, dbPool)
 
 exports.app = app
--- wikiword/public/js/index/index.js
+++ wikiword/public/js/index/index.js
@@ -1,1 +1,34 @@
-document.addEventListener('DOMContentLoaded', function () { keySetting(); indexEventSetting(); wikiWordSignInCheck() })
+function indexEventSetting () {
+
+  // 로그인에 필요한 키 값 세팅함수
+  keySetting();
+  
+  let logo = document.querySelector('.main_div img'),
+    keyboard = document.querySelector('.keyboard')
+
+  logo.addEventListener('click', e => {
+    if (keyboard.classList.contains('long')) {
+
+    } else {
+      keyboard.classList.add('long')
+    }
+  })
+
+  document.addEventListener('keydown', function (e) {
+    console.log(e)
+    if (keyboard.classList.contains('long')) {
+
+    } else {
+      keyboard.classList.add('long')
+    }
+  })
+
+  keyboard.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+      location.href = ('/result?query=' + keyboard.value)
+    }
+  })
+  
+  // 로그인 여부 확인 밑 이름 표시
+  wikiWordSignInCheck()
+}
