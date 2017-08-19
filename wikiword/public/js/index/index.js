cd4e0fa975444091d23f35937e97857417b97878
ywnwalone <ywnwalone@naver.com>
2017-08-19 16:38:02 +0900

    Fix css 2

--- wikiword/public/js/index/index.js
+++ wikiword/public/js/index/index.js
@@ -2,17 +2,17 @@ function indexEventSetting () {
 
   // 로그인에 필요한 키 값 세팅함수
   keySetting();
-  
+
   let logo = document.querySelector('.main_div img'),
     keyboard = document.querySelector('.keyboard')
 
-  logo.addEventListener('click', e => {
-    if (keyboard.classList.contains('long')) {
-
-    } else {
-      keyboard.classList.add('long')
-    }
-  })
+  // logo.addEventListener('click', e => {
+  //   if (keyboard.classList.contains('long')) {
+  //
+  //   } else {
+  //     keyboard.classList.add('long')
+  //   }
+  // })
 
   document.addEventListener('keydown', function (e) {
     console.log(e)
@@ -28,7 +28,7 @@ function indexEventSetting () {
       location.href = ('/result?query=' + keyboard.value)
     }
   })
-  
+
   // 로그인 여부 확인 밑 이름 표시
   wikiWordSignInCheck()
 }
